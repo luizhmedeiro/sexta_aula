3d330ff1b7a06379cacbcec2d2431e4154189657
Usuario de Treinamento 04 <treinamento04@intranet.ciss>
2016-06-02 20:26:50 -0300

    novas atualizacoes

--- aplication/js/controller/MeuController.js
+++ aplication/js/controller/MeuController.js
@@ -1,25 +1,51 @@
 myApp.controller('MeuController', function($scope) {
 	$scope.pessoa = {};
+	$scope.isEdit = false;
+
 	$scope.dados = [{
-		id : 1,
 		nome : 'Luiz',
 		email : 'luiz@ciss.com.br',
 		telefone : '(46)9999-9999'
 	},{
-		id : 2,
 		nome : 'Henrique',
 		email : 'henrique@ciss.com.br',
 		telefone : '(46)8888-8888'
 	},{
-		id : 3,
 		nome : 'Medeiro',
 		email : 'medeiro@ciss.com.br',
 		telefone : '(46)7777-7777'
 	}];
-	
-	$scope.enviaDados = function(pessoa) {
-		var p = angular.copy(pessoa)
+		
+	$scope.enviarDados = function(pessoa) {
+		if($scope.isEdit){
+			atualizar(pessoa)			
+		} else {
+			cadastrar(pessoa)
+		};
+
+		$scope.isEdit = false;		
+	}
+
+	function cadastrar(pessoa) {
+		var p = angular.copy(pessoa);
 		$scope.dados.push(p);
-		$scope.pessoa = {};
+		delete $scope.pessoa;
+	}
+
+	function atualizar(pessoa) {
+		$scope.dados[$scope.idx] = angular.copy(pessoa);
+		delete $scope.pessoa;
+	}
+
+	$scope.excluirItem = function (idx) {
+		$scope.dados.splice(idx, 1);
+		delete $scope.pessoa;
+		$scope.isEdit = false;
+	}
+
+	$scope.editarItem = function (item, idx) {
+		$scope.idx    = idx;
+		$scope.pessoa = angular.copy(item);
+		$scope.isEdit = true;
 	}
 })
